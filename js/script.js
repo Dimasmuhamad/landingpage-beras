08198e74001063f69fcc973c11fb412def1924ec
Dimasmuhamad <2006084@itg.ac.id>
2024-01-19 15:46:34 +0700

    membuat landing page beras

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// toogle
+const navbarNav = document.querySelector(".navbar-nav");
+//hamburger diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klik diluar menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
