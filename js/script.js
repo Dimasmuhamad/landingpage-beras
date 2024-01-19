6ca496402321b364da2edc3667babb787ec28514
Dimasmuhamad <2006084@itg.ac.id>
2024-01-19 21:41:36 +0700

    Pesan committ

--- js/script.js
+++ js/script.js
@@ -4,11 +4,24 @@ const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+// togle search
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
-//klik diluar menghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+//klik diluar 
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button")
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
